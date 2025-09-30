a5b7fa39f1e747eac1c574e599487ed8d51069c9
shams ali <shaikhshams59@gmail.com>
2025-09-30 11:30:14 +0530

    product pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import AboutSection from "@/components/home/about";
 import HeroSection from "@/components/home/hero";
 import ProductsSection from "@/components/home/products";
 import Navbar from "@/components/layout/navbar";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
---
+++ src/app/products/fashion&clothes/page.jsx
@@ -0,0 +1,7 @@
+export default function Home (){
+    return(
+        <div>
+            <h1>this is Fashoin and clothes page</h1>
+        </div>
+    )
+}
