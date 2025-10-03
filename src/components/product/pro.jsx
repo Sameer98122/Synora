45a972c740fb61f8503a4b1cf2306370849a20c4
Atharva252 <atharvabhintade@gmail.com>
2025-10-03 09:11:38 +0530

    s

--- src/components/product/pro.jsx
+++ src/components/product/pro.jsx
@@ -253,6 +253,7 @@ export default function Product() {
                       </button>
                     </div>
                   </div>
+                  
                 </div>
               </div>
             ))
